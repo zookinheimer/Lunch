2c199f10f401e9d039e234cdd799c3d5ecb4b0ce
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-02-16 01:34:44 -0600

    Fixed buttons and queries
    See `TODOs` for outstanding functionality

--- lunch_psg.py
+++ lunch_psg.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 import random
 import sqlite3
 # from datetime import datetime
+from icecream import ic
 # from matplotlib.pyplot import fill
 # from pathlib import Path
 
@@ -28,12 +29,13 @@ roll_options = [
 # TODO: remove theme on buttons
 layout= [
     [sg.Text(r"Click below to find out what's for Lunch")],
-    [sg.Radio('Cheap', "lunch", default=True, key='cheap'), sg.Radio('Normal', "lunch", key='normal')],
-    [sg.Button('Roll Lunch', key='roll', pad=5, use_ttk_buttons=None),
-    sg.Button('Add Restaurant', key='add', pad=5, use_ttk_buttons=None),
-    sg.Button('Delete Restaurant', key='delete', pad=5, use_ttk_buttons=None),
-    sg.Button('List All', key='listall', pad=5, use_ttk_buttons=None),
-    ]
+    [sg.Radio('Cheap', "lunch", key='cheap', default=True),
+    sg.Radio('Normal', "lunch", key='normal')],
+    [sg.Button('Roll Lunch', pad=5, use_ttk_buttons=None),
+    sg.Button('Add Restaurant', pad=5, use_ttk_buttons=None),
+    sg.Button('Delete Restaurant', pad=5, use_ttk_buttons=None),
+    sg.Button('List All', pad=5, use_ttk_buttons=None),
+    ],
 ]
 
 mainscreen = sg.Window(
@@ -44,7 +46,6 @@ mainscreen = sg.Window(
     element_justification='c',
 ).Finalize()
 
-# TODO: debug buttons not showing popup; sqlite function
 while True:
     event, value = mainscreen.Read()
     if event in (None, 'Cancel'):
@@ -54,41 +55,50 @@ while True:
             lunch = "Cheap"
         elif value['normal'] == True:
             lunch = "Normal"
-        restaurant = random.choice(lunch)
-        # random choice from lunch.db
+        # TODO: get list of restaurants from database (cf. `calculate_lunch`)
+        # restaurant = random.choice()
         conn = sqlite3.connect('lunch.db')
         c = conn.cursor()
-        c.execute("SELECT * FROM lunch WHERE lunch_type = ?", (lunch,))
+        c.execute(f"""SELECT * FROM lunch_list WHERE "option" LIKE '{lunch}'""")
         result = c.fetchall()
         conn.close()
+        # TODO: table view
         sg.Popup(result)
+    # TODO: validate input (e.g., empty string, non alphanumeric, etc.)
+    # TODO: escape single quotes in `name` (cf. `'McDonald''s'`)
     if event in (None, 'Add Restaurant'):
-        sg.Popup('Add a restaurant')
-        sg.Popup('Enter the name of the restaurant')
-        name = sg.PopupGetText('Name')
-        sg.Popup('Enter the price of the lunch')
-        price = sg.PopupGetText('Price')
-        sg.Popup('Enter the lunch type')
-        lunch = sg.PopupGetText('Lunch')
-        conn = sqlite3.connect('lunch.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO lunch VALUES (:name, :price, :lunch)",
-                    {'name': name, 'price': price, 'lunch': lunch})
-        conn.commit()
-        conn.close()
+        name = sg.PopupGetText('Enter the name of the restaurant')
+        while name.isalnum() == False or name.isalpha() == False:
+            sg.Popup("Please enter a valid restaurant name")
+            name = sg.PopupGetText('Enter the name of the restaurant')
+
+        price = (sg.PopupGetText('Cheap or Normal')).lower()
+        while price != 'cheap' or price != 'normal':
+            sg.Popup("Please enter a valid type of restaurant")
+            price = sg.PopupGetText('Cheap or Normal')
+
+        try:
+            conn = sqlite3.connect('lunch.db')
+            c = conn.cursor()
+            c.execute("INSERT INTO lunch_list VALUES (:restaurants, :option)",
+                        {'restaurants': name, 'option': price})
+            conn.commit()
+            conn.close()
+            sg.Popup("Restaurant added successfully")
+        except sqlite3.IntegrityError:
+            sg.Popup('Restaurant already added')
     if event in (None, 'Delete Restaurant'):
-        sg.Popup('Delete a restaurant')
-        sg.Popup('Enter the name of the restaurant')
-        name = sg.PopupGetText('Name')
+        name = sg.PopupGetText('Delete a restaurant')
         conn = sqlite3.connect('lunch.db')
         c = conn.cursor()
-        c.execute("DELETE FROM lunch WHERE name = :name", {'name': name})
+        c.execute(f"""DELETE FROM lunch_list WHERE "restaurants" = '{name}'""")
         conn.commit()
         conn.close()
     if event in (None, 'List All'):
         conn = sqlite3.connect('lunch.db')
         c = conn.cursor()
-        c.execute("SELECT *, oid FROM lunch")
+        c.execute("SELECT *, oid FROM lunch_list")
         results = c.fetchall()
         conn.close()
+        # TODO: table view
         sg.Popup(results)
